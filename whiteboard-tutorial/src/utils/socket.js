6067cf1531d047b50413188771520bf0a81eb303
Sumit Kumar Sharma <sksharma372@kgpian.iitkgp.ac.in>
2025-02-25 03:04:00 +0530

    api udated

--- whiteboard-tutorial/src/utils/socket.js
+++ whiteboard-tutorial/src/utils/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const token = localStorage.getItem("whiteboard_user_token");
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://api-whiteboard-az.onrender.com", {
   extraHeaders: token ? { Authorization: `Bearer ${token}` } : {}, // Only send if token exists
 });
 
